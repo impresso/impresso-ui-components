06ebd26c8eea4962b9b1f6628a5be0a636a17b76
Daniele Guido <gui.daniele@gmail.com>
2025-05-23 11:18:45 +0200

    add SignUpForm and remove unused storybook addon

---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,326 @@
+<template>
+  <form class="SignUpForm container">
+    <BFormGroup
+      id="input-group-1"
+      label="Email (please use institution email if available) *"
+      label-for="email"
+      :description="(v$.email.$errors[0]?.$message as string)"
+    >
+      <BFormInput
+        id="email"
+        name="email"
+        type="email"
+        required
+        autocomplete="email"
+        :class="{
+          'border-danger': v$.email.$error,
+          'border-success': !v$.email.$error && v$.email.$dirty,
+        }"
+        class="rounded-sm shadow-sm"
+        v-model.trim="formData.email"
+      ></BFormInput>
+    </BFormGroup>
+    <p class="text-muted very-small pt-0 px-3">
+      Note: for Student User or Academic User plans, your institution email
+      address is
+      <b>required</b>.
+    </p>
+    <div class="row">
+      <div class="col">
+        <BFormGroup
+          label="Affiliation"
+          label-for="affiliation"
+          :description="(v$.affiliation?.$errors[0]?.$message as string)"
+        >
+          <bFormInput
+            id="affiliation"
+            name="affiliation"
+            :required="doesPlanRequireAffiliation"
+            v-model.trim="formData.affiliation"
+            :class="{
+              'border-danger': v$.affiliation?.$error,
+              'border-success':
+                !v$.affiliation?.$error && v$.affiliation?.$dirty,
+            }"
+            class="rounded-sm shadow-sm"
+            placeholder="University, Company, ..."
+          ></bFormInput>
+        </BFormGroup>
+      </div>
+      <div class="col">
+        <BFormGroup
+          label="Institutional URL"
+          label-for="institutional-url"
+          :description="(v$.institutionalUrl?.$errors[0]?.$message as string)"
+        >
+          <bFormInput
+            id="institutional-url"
+            name="institutional-url"
+            type="url"
+            :required="doesPlanRequireAffiliation"
+            autocomplete="url"
+            v-model.trim="formData.institutionalUrl"
+            :class="{
+              'border-danger': v$.institutionalUrl?.$error,
+              'border-success':
+                !v$.institutionalUrl?.$error && v$.institutionalUrl?.$dirty,
+            }"
+            class="rounded-sm shadow-sm"
+            placeholder="https://"
+          ></bFormInput>
+        </BFormGroup>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <BFormGroup
+          label="First Name *"
+          label-for="firstname"
+          :description="(v$.firstname?.$errors[0]?.$message as string)"
+        >
+          <bFormInput
+            id="firstname"
+            name="firstname"
+            required
+            autocomplete="url"
+            v-model.trim="formData.firstname"
+            :class="{
+              'border-danger': v$.firstname?.$error,
+              'border-success': !v$.firstname?.$error && v$.firstname?.$dirty,
+            }"
+            class="rounded-sm shadow-sm"
+            placeholder="First Name"
+          ></bFormInput>
+        </BFormGroup>
+      </div>
+      <div class="col">
+        <BFormGroup
+          label="Last Name *"
+          label-for="lastname"
+          :description="(v$.lastname?.$errors[0]?.$message as string)"
+        >
+          <bFormInput
+            id="lastname"
+            name="lastname"
+            :required="doesPlanRequireAffiliation"
+            autocomplete="url"
+            v-model.trim="formData.lastname"
+            :class="{
+              'border-danger': v$.lastname?.$error,
+              'border-success': !v$.lastname?.$error && v$.lastname?.$dirty,
+            }"
+            class="rounded-sm shadow-sm"
+            placeholder="Last Name *"
+          ></bFormInput>
+        </BFormGroup>
+      </div>
+    </div>
+    <!-- password -->
+    <div class="row" v-if="!hasInitialValues">
+      <div class="col">
+        <BFormGroup
+          label="Password *"
+          label-for="firstname"
+          :description="(v$.password.$errors[0]?.$message as string)"
+        >
+          <bFormInput
+            id="password"
+            name="password"
+            required
+            type="text"
+            autocomplete="password"
+            v-model.trim="formData.password"
+            :class="{
+              'border-danger': v$.password.$error,
+              'border-success': !v$.password.$error && v$.password.$dirty,
+            }"
+            class="rounded-sm shadow-sm"
+            placeholder="Password"
+          ></bFormInput>
+        </BFormGroup>
+      </div>
+      <div class="col">
+        <BFormGroup
+          label="Repeat password *"
+          label-for="repeatPassword"
+          :description="(v$.repeatPassword?.$errors[0]?.$message as string)"
+        >
+          <bFormInput
+            id="repeatPassword"
+            name="repeatPassword"
+            type="text"
+            :required="doesPlanRequireAffiliation"
+            autocomplete="new-password"
+            v-model.trim="formData.repeatPassword"
+            :class="{
+              'border-danger': v$.repeatPassword?.$error,
+              'border-success':
+                !v$.repeatPassword?.$error && v$.repeatPassword?.$dirty,
+            }"
+            class="rounded-sm shadow-sm"
+            placeholder="Last Name *"
+          ></bFormInput>
+        </BFormGroup>
+      </div>
+    </div>
+    <!-- error ma,nagemend and form submission -->
+    <div class="position-sticky bottom-0 bg-white border-top py-3">
+      <slot name="form-errors">
+        <Alert type="warning" class="mb-3" role="alert" v-if="v$.$error">
+          <p class="m-0">
+            Please correct the errors in the form before submitting.
+          </p>
+        </Alert>
+      </slot>
+      <slot name="submit-button" v-bind:submit="submitForm">
+        <button
+          class="btn btn-outline-secondary btn-md px-4 border border-dark btn-block"
+          type="button"
+          @click="submitForm"
+          :disabled="isLoading"
+          aria-label="Register"
+        >
+          <Icon name="key" :strokeWidth="1.5" />
+          <span class="ml-2" v-if="isLoading">Saving...</span>
+          <span class="ml-2" v-else-if="hasInitialValues">Save changes</span>
+          <span class="ml-2" v-else>Create Account</span>
+        </button>
+      </slot>
+    </div>
+  </form>
+</template>
+<script setup lang="ts">
+import useVuelidate from '@vuelidate/core'
+import Icon from './Icon.vue'
+import Alert from './Alert.vue'
+import {
+  email,
+  helpers,
+  minLength,
+  required,
+  sameAs,
+} from '@vuelidate/validators'
+import BFormGroup from './legacy/BFormGroup.vue'
+import BFormInput from './legacy/BFormInput.vue'
+import { reactive, computed } from 'vue'
+
+export interface SignUpFormPayload {
+  firstname: string
+  lastname: string
+  email: string
+  password: string
+  repeatPassword: string
+  institutionalUrl: string
+  affiliation: string
+}
+
+export interface SignUpFormProps {
+  initialValues?: SignUpFormPayload
+  doesPlanRequireAffiliation?: boolean
+  isLoading?: boolean
+}
+
+const props = withDefaults(defineProps<SignUpFormProps>(), {
+  initialValues: () => ({
+    firstname: '',
+    lastname: '',
+    institutionalUrl: '',
+    affiliation: '',
+    email: '',
+    password: '',
+    repeatPassword: '',
+  }),
+})
+const hasInitialValues = computed(() => {
+  return (
+    Object.keys(props.initialValues).reduce(
+      (acc, key) =>
+        (acc += (
+          props.initialValues[key as keyof SignUpFormPayload] ?? ('' as string)
+        ).length),
+      0
+    ) > 0
+  )
+})
+// const showNewPassword = ref(false)
+// const showRepeatPassword = ref(false)
+// Form data
+const formData = reactive<SignUpFormPayload>(props.initialValues)
+const PasswordRegex =
+  /^(?=.*?[A-Z])(?=.*[a-z])(?=.*[\d])(?=.*[\W_])(?!.*\s).{8,}$/
+// Vuelidate rules
+const formRules = computed(() => {
+  const passwordRules = hasInitialValues.value
+    ? {}
+    : {
+        password: {
+          minLength: minLength(8),
+          urlRegex: helpers.withMessage(
+            'Password must contain at least one uppercase letter, one lowercase letter, one number and one special character',
+            (value: string) => PasswordRegex.exec(value) != null
+          ),
+          required,
+          $autoDirty: true,
+        }, // min: 8, regex: passwordRegex
+        repeatPassword: {
+          required,
+          sameAsPassword: helpers.withMessage(
+            'Passwords do not match.',
+            sameAs(computed(() => formData.password))
+          ),
+          $autoDirty: true,
+        }, // required|confirmed:repeatPassword
+      }
+  return {
+    firstname: { required, minLength: minLength(2), $autoDirty: true }, // required|min:2
+    lastname: { required, minLength: minLength(2), $autoDirty: true }, // required|min:2
+    email: { required, minLength: minLength(4), email, $autoDirty: true }, // required|email
+
+    institutionalUrl: {
+      $autoDirty: true,
+      required: false,
+      urlRegex: helpers.withMessage(
+        'Please enter a valid URL',
+        (value: string) => {
+          if (!value || value.length === 0) {
+            return true
+          }
+          const urlPattern =
+            /^(https?:\/\/)?([\w\-]+\.)+[\w\-]+(\/[\w\-._~:/?#[\]@!$&'()*+,;=]*)?$/
+          return urlPattern.test(value)
+        }
+      ),
+    },
+    affiliation: {
+      $autoDirty: true,
+      required: false,
+      minLength: minLength(2),
+    },
+    ...passwordRules,
+  }
+})
+
+// Initialize validation
+const v$ = useVuelidate(formRules, formData)
+
+// Define emits with type safety
+const emit = defineEmits<{
+  (e: 'submit', payload: SignUpFormPayload): void
+}>()
+
+// Form submission handler
+const submitForm = async () => {
+  // Trigger validation
+  const isFormValid = await v$.value.$validate()
+  console.info('[SignUpForm] Form validation result:', isFormValid)
+  if (isFormValid) {
+    // Form is valid, emit event with password data
+    emit('submit', {
+      ...formData,
+    })
+  } else {
+    // log the errors
+    console.error('[SignUpForm] Form validation errors:', v$.$errors)
+  }
+}
+</script>
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
-export { default as Alert } from "./components/Alert.vue";
-export { default as Icon } from "./components/Icon.vue";
+export { default as Alert } from './components/Alert.vue'
+export { default as Icon } from './components/Icon.vue'
+export { default as ChangePasswordForm } from './components/ChangePasswordForm.vue'
